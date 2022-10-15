eaa3db23029df9942e56827bb63f4a70b3ab3520
Adegoke Temitope <adegoketemitope1909@gmail.com>
2022-10-15 12:20:09 +0100

    feat: use props

--- src/components/Cards/SNKRSPricingCard/SNKRSPricingCard.js
+++ src/components/Cards/SNKRSPricingCard/SNKRSPricingCard.js
@@ -1,17 +1,17 @@
 import style from "./SNKRSPricingCard.module.css";
 import Image1 from "../../../assets/images/SNKRS/ec50ad80-da49-4064-af84-1d9fb34b71de.webp";
 
-const SNKRSPricingCard = () => {
+const SNKRSPricingCard = ({ product }) => {
   return (
     <div className={style.container}>
       <div className={style.imageWrapper}>
-        <img className={style.image} src={Image1} alt="image1" />
+        <img className={style.image} src={product.img} alt={product.img} />
       </div>
       <div className={style.content}>
-        <p className={style.tag}>SNKRS Exclusives</p>
-        <h4 className={style.name}>Air Jordan 1 Utility</h4>
-        <p className={style.category}>Men's Shoe</p>
-        <p className={style.price}>$160</p>
+        <p className={style.tag}>{product.tag}</p>
+        <h4 className={style.name}>{product.name}</h4>
+        <p className={style.category}>{product.category}</p>
+        <p className={style.price}>{product.price}</p>
       </div>
     </div>
   );
